4ee5bfb087c40ed28901cc202d8191547b0e5b3b
abhishadaine <daine.a@northeastern.edu>
2022-11-30 16:30:01 -0500

    Integrating Node.js with MongoDB Atlas

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
-mongoose.connect('mongodb://localhost:27017/tuiter');
+// mongoose.connect('mongodb://localhost:27017/tuiter');
 
-// const CONNECTION_STRING = "mongodb+srv://abhisha:Abhish%4006@cluster0.xqznmb6.mongodb.net/?retryWrites=true&w=majority"
-//     || 'mongodb://localhost:27017/tuiter'
-// mongoose.connect(CONNECTION_STRING);
-//
+const CONNECTION_STRING = "mongodb+srv://abhisha:Abhish%4006@cluster0.xqznmb6.mongodb.net/?retryWrites=true&w=majority"
+    || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
+
 
 const app = express()
 app.use(express.json());
